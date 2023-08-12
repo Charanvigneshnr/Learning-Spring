a35285151fc02705b568b6d741d019bdf674d088
Charanvigneshnr <charanvignesh10@gmail.com>
2023-08-12 22:45:24 +0530

    Learning Spring Framework

--- src/main/java/com/charan/learnspringframework/App03GamingSpring.java
+++ src/main/java/com/charan/learnspringframework/App03GamingSpring.java
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App03GamingSpring {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
-        context.getBean(GamingConsole.class).right();
+        context.getBean(GamingConsole.class).up();
     }
 }
